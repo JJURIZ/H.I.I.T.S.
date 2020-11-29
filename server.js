7466451cdb3903ff3dc55ca0d62883dae249c154
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-29 09:52:03 -0500

    init commit

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3001;
 const server = app.listen(PORT, () => {
-  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
+  console.log(`Listening on ${PORT}`);
 });
 
 module.exports = server;
