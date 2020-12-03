05eb145752b513f4ad0dd72b2c9da5662305a5d0
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-12-03 11:53:53 -0500

    removed comment

--- routes/user.js
+++ routes/user.js
@@ -10,78 +10,77 @@ const clientSecret = process.env.CLIENT_SECRET;
 const authKey = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
 
 // USER HOMEPAGE
-router.get('/', (req, res) => {
-    db.fave.findAll()
-    .then((faves) => {
-  res.render('user', { faves, tracks: [] });
+router.get("/", (req, res) => {
+  db.fave.findAll()
+  .then((faves) => {
+    res.render("user", { faves, tracks: [] });
+  });
 });
-});
-
-
-// router.get('/faveTest', (req, res) => {
-//     db.fave.findAll()
-//     .then((faves) => {
-//         res.render('faveTest', { faves });
-//     })
-//   });
-
 
 // FIND SONGS
-router.get('/:track', (req, res) => {
-    axios.post('https://accounts.spotify.com/api/token',
-    querystring.stringify({
-        grant_type: 'client_credentials'
-    }),
-    {
+router.get("/:track", (req, res) => {
+  axios
+    .post(
+      "https://accounts.spotify.com/api/token",
+      querystring.stringify({
+        grant_type: "client_credentials",
+      }),
+      {
         headers: {
-            Authorization: `Basic ${authKey}`
-        }
-    })
+          Authorization: `Basic ${authKey}`,
+        },
+      }
+    )
     .then((response) => {
-        let token = response.data.access_token;
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }
-        let track = encodeURIComponent(req.query.track)
-        axios.get(`https://api.spotify.com/v1/search?q=${track}&type=track,track&offset=0&limit=10`, config)
+      let token = response.data.access_token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      let track = encodeURIComponent(req.query.track);
+      axios
+        .get(
+          `https://api.spotify.com/v1/search?q=${track}&type=track,track&offset=0&limit=10`,
+          config
+        )
         .then((response) => {
-            let tracks = response.data.tracks.items
-            res.render('user', { tracks })
+          let tracks = response.data.tracks.items;
+          res.render("user", { tracks });
         })
         .catch((err) => {
-            console.log(err)
-        })
-    })
-})
+          console.log(err);
+        });
+    });
+});
 
 // ADD TRACK TO FAVORITES
-router.post('/', (req, res) => {
-    db.track.findOrCreate({
-        where: { spotify_id: req.body.id },
-        defaults: {
-            title: req.body.title,
-            artist: req.body.artist,
-            durationMs: req.body.durationMs,
-            explicit: req.body.explicit,
-            preview_url: req.body.preview_url
-        }
+router.post("/", (req, res) => {
+  db.track
+    .findOrCreate({
+      where: { spotify_id: req.body.id },
+      defaults: {
+        title: req.body.title,
+        artist: req.body.artist,
+        durationMs: req.body.durationMs,
+        explicit: req.body.explicit,
+        preview_url: req.body.preview_url,
+      },
     })
     .then((fave) => {
-       db.fave.findOrCreate({
-           where: { 
-               spotify_id: fave[0].spotify_id,
-               userId: req.session.passport.user 
-            }
-       })
+      db.fave.findOrCreate({
+        where: {
+          spotify_id: fave[0].spotify_id,
+          userId: req.session.passport.user,
+        },
+      });
     })
     .catch((err) => {
-        console.log(err)
+      console.log(err);
     })
-    .then(() => { 
-        res.redirect('/user')
-    })
+    .then(() => {
+      res.redirect("/user");
+    });
 });
 
 module.exports = router;
